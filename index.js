0360afdb580cc943d824d415ef326a0422f4779c
Fernanda Avalos <fernanda.avalos2002@gmail.com>
2022-10-10 20:01:16 -0500

    Clase 10 de octubre

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => ( //Cuando algo llegue responder hello world
     res.send('Hello World')
 ));
 
+//Create
 app.post('/create',async(req, res)=>{
     try {
         const {body:movie}=req //Alias al body llamada movie
@@ -38,5 +39,73 @@ app.post('/create',async(req, res)=>{
         res.send(error)
     }
 })
+
+//READ
+app.get('/get-movie/:id', async (req, res) => { //A todo lo que llegue a get movie lo vas a mandar aquí y lo que llegue después se guarda en id
+    try {
+        const {params:{id}}=req 
+        console.log('id', id)
+        const doc = db.collection('movies').doc(id)
+        const {_fieldsProto} = await doc.get()
+        res.send({
+            status: 200,
+            time: _fieldsProto.time.stringValue,
+            author: _fieldsProto.author.stringValue,
+            name: _fieldsProto.name.stringValue,
+            rating: _fieldsProto.rating.stringValue
+        })
+    } catch(error) {
+        res.send(error)
+    }
+})
+
+//DELETE
+app.delete('/delete-movie/:id', async (req, res) => { //A todo lo que llegue a get movie lo vas a mandar aquí y lo que llegue después se guarda en id
+    try {
+        const {params: {id}} = req
+        const movieDB = db.collection('movies').doc(id)
+        await movieDB.delete()
+        res.send({
+            status: 200
+        })
+    } catch(error) {
+        res.send(error)
+    }
+})
+
+//UPDATE
+app.put('/update-movie/', async (req, res) => { //A todo lo que llegue a get movie lo vas a mandar aquí y lo que llegue después se guarda en id
+    try {
+        const {body: movie} = req
+        const { time, author, name, rating} = movie
+        const movieDB = db.collection('movies').doc(id)
+        const resp = await movieDB.update({
+            name,
+            time,
+            rating,
+            author
+        })
+        res.send({
+            status: 200,
+            id,
+        })
+    } catch(error) {
+        res.send(error)
+    }
+})
+
+//Get-movies
+app.get('/get-movies', async (req, res) => { //A todo lo que llegue a get movie lo vas a mandar aquí y lo que llegue después se guarda en id
+    try {
+        const moviesDB = await db.collection('movies').get()
+        const resp=moviesDB.docs.map(doc=>doc.data())
+        res.send({
+            resp
+        })
+    } catch(error) {
+        res.send(error)
+    }
+})
+
 
 app.listen(apiPort, ()=> console.log(`Server running on port ${apiPort}`)); //Cuando acabe de levantarse la aplicación se quede dormida hasta request
